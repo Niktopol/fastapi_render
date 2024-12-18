93a1ef77175f68c56aae1feb204e69d1343ec570
Niktopol <74873218+Niktopol@users.noreply.github.com>
2024-12-18 13:35:26 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = 'postgresql://user:a9QtBSozWH4S8Gjrz5LgzXNGidHH1XcD@dpg-csh6nmhu0jms739sl7p0-a.frankfurt-postgres.render.com/fastapi_database_rmyd'
+DATABASE_URL = 'postgresql://user:qO8WGcxbV0pfsir1ttjHOx8vCsf3ySUt@dpg-ctha8elumphs73fm56ag-a.frankfurt-postgres.render.com/fastapi_database_7fn6'
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush = False, bind=engine)
